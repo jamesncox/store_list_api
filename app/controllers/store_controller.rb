ce0a0dcd2fbe7d8de227e47d6ea09aa25d660e8b
jamesncox <jamesncox@yahoo.com>
2019-10-30 14:32:18 -0500

    Add index action Store

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
 
+    def index
+        @store = Store.all 
+        render json: StoreSerializer.new(@store)
+    end
+
     def show
         @store = Store.find_by(id: params[:id])
         options = {
