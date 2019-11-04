a06359033ee7b34ebce85853b066c0c3ce4d2503
jamesncox <jamesncox@yahoo.com>
2019-11-04 14:33:18 -0600

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-3.times do 
-    Store.create({name: "Kroger"})
-end
+# 3.times do 
+#     Store.create({name: "Kroger"})
+# end
 
-3.times do 
-    List.create({name: "Monday", price: 3.99, quantity: "100", store_id: 1})
-end 
+# 3.times do 
+#     List.create({item: "Monday", price: 3.99, quantity: "100", store_id: 1})
+# end 
+
+stores = Store.create([{ name: 'Kroger' }, { name: 'Target' }, { name: 'Lucky' }])
+List.create(item: 'Milk', price: 3.99, quantity: 1, store_id: 1, store: stores.first)
+List.create(item: 'Quilt', price: 70.00, quantity: 2, store_id: 2, store: stores[1])
+List.create(item: 'Bomber Jacket', price: 150.69, quantity: 1, store_id: 3, store: stores[2])
